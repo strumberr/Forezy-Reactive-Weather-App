9a3109c8452417527e44d5c4bd3a676b667275b7
storm <stormurbiz@gmail.com>
2023-06-16 15:37:42 +0200

    PWA25

--- src/pages/500.js
+++ src/pages/500.js
@@ -1,17 +1,20 @@
+import { useEffect } from 'react';
 import { useRouter } from 'next/router';
 
 export default function Custom404() {
-  const router = useRouter();
+  useEffect(() => {
+    const router = useRouter();
 
-  function handleClick() {
-    router.push('/');
-  }
+    const handleClick = () => {
+      router.push('/');
+    };
 
-  function scheduleRefresh() {
-    setTimeout(handleClick, 0 * 1 * 1000);
-  }
+    function scheduleRefresh() {
+      setTimeout(handleClick, 0 * 1 * 1000);
+    }
 
-  scheduleRefresh();
+    scheduleRefresh();
+  }, []);
 
-  return <h1>404 - Page Not Found</h1>;
+  return <h1>500 - Server error</h1>;
 }
