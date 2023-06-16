298821720835866366d14329ac17c75d0ccebb84
storm <stormurbiz@gmail.com>
2023-06-16 15:31:38 +0200

    PWA23

--- src/pages/500.js
+++ src/pages/500.js
@@ -1,19 +1,22 @@
 import { useRouter } from 'next/router';
+import { useEffect } from 'react';
 
 
 export default function Custom500() {
 
-    const router = useRouter();
+    useEffect(() => {
+        const router = useRouter();
 
-    const handleClick = () => {
-        router.push('/');
-    };
-    
-    function scheduleRefresh() {
-        setTimeout(handleClick, 0 * 1 * 1000); // Refresh after 40 minutes (40 minutes * 60 seconds * 1000 milliseconds)
-    }
+        const handleClick = () => {
+            router.push('/');
+        };
+        
+        function scheduleRefresh() {
+            setTimeout(handleClick, 0 * 1 * 1000); // Refresh after 40 minutes (40 minutes * 60 seconds * 1000 milliseconds)
+        }
 
-      scheduleRefresh();
+        scheduleRefresh();
+    }, []);
 
     return <h1>500 - Server-side error occurred</h1>
 }
